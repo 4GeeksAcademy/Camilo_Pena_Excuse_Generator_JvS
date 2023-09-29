f4316f824aee66621e3d9539b1ff7e6a4b2b4552
KmiloPena <125616941+KmiloPena@users.noreply.github.com>
2023-09-29 23:44:41 +0000

    Excuse Generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,35 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  function excuseGenerator() {
+    let who = ["The dog", "The kraken", "Jack Sparrow", "Sponge Bob"];
+    let action = ["ate", "peed", "crushed", "broke", "took"];
+    let what = ["my homework", "the keys", "the car", "my bike"];
+    let when = [
+      "today",
+      "last week",
+      "right now",
+      "last month",
+      "one hour ago"
+    ];
+
+    let whoRandom = Math.floor(Math.random() * who.length);
+    let actionRandom = Math.floor(Math.random() * action.length);
+    let whatRandom = Math.floor(Math.random() * what.length);
+    let whenRandom = Math.floor(Math.random() * when.length);
+
+    return (
+      who[whoRandom] +
+      " " +
+      action[actionRandom] +
+      " " +
+      what[whatRandom] +
+      " " +
+      when[whenRandom]
+    );
+  }
+  let excuse = excuseGenerator();
+  document.getElementById("excuse").innerHTML = excuse;
+
+  console.log(excuse);
 };
